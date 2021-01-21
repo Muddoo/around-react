739278cbff6e62de8ce1b7c5759cb3edd265b57b
Moddoo <>
2021-01-21 06:46:26 -0600

    Working On Api

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,7 +1,6 @@
 import {useState,useEffect} from 'react'
 function ImagePopup(props) {
     const {card,onClose} = props;
-    console.log(card)
     return (
         <figure className={`popup popup_figure ${card._id ? 'visible' : null}`} onClick={onClose}>
             <div className="popup__container">
