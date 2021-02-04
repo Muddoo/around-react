fc1b4853aad78c9323d73f86621cd38ea16be091
Moddoo <>
2021-02-03 23:09:55 -0600

    done

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-function Header(props) {
-    return (
-        <header className="header">
-            <img draggable="false" src={props.logo} alt="logo" className="logo" />
-        </header>
-    )
-}
-
-export default Header
